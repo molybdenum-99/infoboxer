9e641d9bfff9662d5595aa830a332cf2e87ccf13
zverok <zverok.offline@gmail.com>
2015-08-06 19:05:44 +0300

    Templates pseudo-doc

--- lib/infoboxer/templates.rb
+++ lib/infoboxer/templates.rb
@@ -1,4 +1,7 @@
 module Infoboxer
+  # Templates are cool, powerful and undocumented. Sorry :(
+  #
+  # I do my best.
   module Templates
     %w[base set].each do |tmpl|
       require_relative "templates/#{tmpl}"
