16a2d31d517072b2e8064ce26829a0c171a97b64
zverok <zverok.offline@gmail.com>
2015-08-07 12:01:08 +0300

    MediaWiki::Page docs

--- lib/infoboxer/media_wiki/page.rb
+++ lib/infoboxer/media_wiki/page.rb
@@ -1,13 +1,28 @@
 # encoding: utf-8
 module Infoboxer
   class MediaWiki
+    # A descendant of {Tree::Document Document}, representing page,
+    # received from {MediaWiki} client.
+    #
+    # Alongside with document tree structure, knows document's title as
+    # represented by MediaWiki and human (non-API) URL.
     class Page < Tree::Document
       def initialize(client, children, raw)
         @client = client
         super(children, raw)
       end
 
+      # Instance of {MediaWiki} which this page was received from
+      # @return {MediaWiki}
       attr_reader :client
+
+      # @!attribute [r] title
+      #   Page title.
+      #   @return [String]
+
+      # @!attribute [r] url
+      #   Page friendly URL.
+      #   @return [String]
 
       def_readers :title, :url, :traits
 
