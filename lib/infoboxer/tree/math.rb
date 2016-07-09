8bb390e7b1f99289ed50fb64ffbd9c9d74f5458f
zverok <zverok.offline@gmail.com>
2016-07-09 16:07:21 +0300

    Fix #68 (Math#text)

--- lib/infoboxer/tree/math.rb
+++ lib/infoboxer/tree/math.rb
@@ -4,6 +4,9 @@ module Infoboxer
     #
     # See also: https://en.wikipedia.org/wiki/Help:Displaying_a_formula
     class Math < Text
+      def text
+        "<math>#{super}</math>"
+      end
     end
   end
 end
