637c1385a241c76c36509ef80b06c7c609d00c0b
zverok <zverok.offline@gmail.com>
2017-09-16 04:44:08 +0300

    Cop

--- lib/infoboxer/version.rb
+++ lib/infoboxer/version.rb
@@ -4,6 +4,6 @@ module Infoboxer
   MAJOR = 0
   MINOR = 3
   PATCH = 0
-  PRE = 'pre' # set to `nil` for normal releases
+  PRE = 'pre'.freeze # set to `nil` for normal releases
   VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
 end
